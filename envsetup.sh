90a34f06fc56e0a606c25fc938b723b28eda0b93
podxboq <podxboq@gmail.com>
2012-02-09 11:34:38 +0100

    Define lunch enviroment.

--- envsetup.sh
+++ envsetup.sh
@@ -403,11 +403,6 @@ function add_lunch_combo()
     LUNCH_MENU_CHOICES=(${LUNCH_MENU_CHOICES[@]} $new_combo)
 }
 
-# add the default one here
-add_lunch_combo full-eng
-add_lunch_combo full_x86-eng
-add_lunch_combo vbox_x86-eng
-
 function print_lunch_menu()
 {
     local uname=$(uname)
@@ -436,6 +431,44 @@ function print_lunch_menu()
     fi
 
     echo
+}
+
+function lunch()
+{
+    local answer
+    if [ "$1" ] ; then
+        answer=$1
+    else
+        print_lunch_menu
+        echo -n "Which would you like? [full-eng] "
+        read answer
+    fi
+
+    local selection=
+
+    if (echo -n $answer | grep -q -e "^[0-9][0-9]*$")
+    then
+        if [ $answer -le ${#LUNCH_MENU_CHOICES[@]} ]
+        then
+            selection=${LUNCH_MENU_CHOICES[$(($answer-1))]}
+        fi
+    elif (echo -n $answer | grep -q -e "^[^\-][^\-]*-[^\-][^\-]*$")
+    then
+        selection=$answer
+    fi
+
+    if [ -z "$selection" ]
+    then
+        echo
+        echo "Invalid lunch combo: $answer"
+        return 1
+    fi
+
+    export TARGET_BUILD_APPS=
+
+    echo
+    set_stuff_for_environment
+    printconfig
 }
 
 function gettop
